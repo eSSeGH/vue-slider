9ffd1035a2728f61515e20f1248dd8ddb9cebafa
eSSeGH <ste.virgadaula@gmail.com>
2023-02-20 18:04:01 +0100

    EDIT+JS change slide feat added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const { createApp } = Vue
 
-let indexSlide = 0
-console.log(indexSlide)
+let slideIndex = 0
 
 createApp({
     data() {
@@ -9,28 +8,30 @@ createApp({
             message: 'Vue Slider',
             headerClass: 'header',
             h1Class: 'title-header',
-            slideClass: 'slide',
-            activeClass: 'active',
             slideOverlayClass: 'slide-overlay',
-            slideEls: [document.querySelectorAll(".slide")],
             images: [
                 {
+                    class: ['slide', 'active'],
                     imageUrl: './img/01.webp',
                     title: "Marvel's Spiderman Miles Morales",
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                 }, {
+                    class: 'slide',
                     imageUrl: './img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                 }, {
+                    class: 'slide',
                     imageUrl: './img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 }, {
+                    class: 'slide',
                     imageUrl: './img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 }, {
+                    class: 'slide',
                     imageUrl: './img/05.webp',
                     title: "Marvel's Avengers",
                     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
@@ -39,48 +40,27 @@ createApp({
         }
     },
     methods: {
-        nextSlide () {
-            console.log("next slide")
+        nextSlide (index) {
+            index = slideIndex
 
-            console.log(slideEls)
+            this.images[index].class = "slide"
+            console.log(this.images[index].class)
 
-            // nascondere la slide corrente
-            let currentSlide = slideEls[indexSlide]
-            console.log(currentSlide)
-            currentSlide.classList.remove("active")
+            slideIndex += 1
 
-            // controllo slide limite destro
-            if (indexSlide < slideEls.length - 1) {
-                indexSlide++
-            } else {
-                indexSlide = 0
-            }
-
-            // mostrare la slide successiva
-            let nextSlide = slideEls[indexSlide]
-            console.log(nextSlide)
-            nextSlide.classList.add("active")                    
+            this.images[index +1 ].class = ["slide", "active"]
+            console.log(this.images[index + 1].class)
         },
-        precSlide() {
-            console.log("prec slide")
+        precSlide(index) {
+            index = slideIndex
 
-            // nascondere la slide corrente
-            let currentSlide = slideEls[indexSlide]
-            console.log(currentSlide)
-            currentSlide.classList.remove("active")
+            this.images[index].class = "slide"
+            console.log(this.images[index].class)
 
-            // controllo slide limite sinistro
-            if (indexSlide > 0) {
-                indexSlide--
-            } else {
-                indexSlide = slideEls.length - 1
-            }
+            slideIndex -= 1
 
-            // mostrare la slide precedente
-            let precSlide = slideEls[indexSlide]
-            console.log(precSlide)
-            precSlide.classList.add("active")
-
+            this.images[index - 1].class = ["slide", "active"]
+            console.log(this.images[index - 1].class)
         }
     }
 }).mount('#app')
